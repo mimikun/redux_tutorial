3a4471ecff048204ceeb81a83148a444be02e4de
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-04 22:21:22 +0900

    冗長な記述を簡略化した

--- reducer.js
+++ reducer.js
@@ -10,6 +10,30 @@ const initialState = {
   todos: []
 }
 
+function todos (state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
 function todoApp (state = initialState, action) {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
@@ -18,23 +42,11 @@ function todoApp (state = initialState, action) {
       })
     case ADD_TODO:
       return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
-        ]
+        todos: todos(state.todos, action)
       })
     case TOGGLE_TODO:
       return Object.assign({}, state, {
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return Object.assign({}, todo, {
-              completed: !todo.completed
-            })
-          }
-          return todo
+        todos: todos(state.todos, action)
         })
       })
     default:
