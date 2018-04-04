13b953f194762cc247f7b358bf051ca2b62e7e16
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-04 21:17:43 +0900

    TOGGLE_TODOを書いた

--- reducer.js
+++ reducer.js
@@ -26,6 +26,17 @@ function todoApp (state = initialState, action) {
           }
         ]
       })
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            })
+          }
+          return todo
+        })
+      })
     default:
       return state
   }
