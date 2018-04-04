b4e1e967dfe77272c9ad4a12d7cb7f7e500b2c0b
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-04 13:05:31 +0900

    todoApp関数を定義した

--- reducer.js
+++ reducer.js
@@ -6,7 +6,12 @@ const initialState = {
 }
 
 function todoApp (state = initialState, action) {
-  //  今のところ、アクションを処理しないでください
-  //  与えられた状態を返します。
-  return state
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      })
+    default:
+      return state
+  }
 }
